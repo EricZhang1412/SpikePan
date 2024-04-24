b7397ede0e46e1a5e8b091d35a376877d01f503d
Jiyuan ZHANG <86053421+EricZhang1412@users.noreply.github.com>
2024-04-24 22:05:04 +0800

    add energy computation

--- fusionnet_test.py
+++ fusionnet_test.py
@@ -10,7 +10,10 @@ from net_struct import \
     Inception_SFusionNet_with_tEBN_BN, \
     Inception_SFusionNet_ori_with_TEBN_2_ATAN, \
     sp_ori_TEBN_2_ATAN_multhead, \
-    Inception_SFusionNet_with_tdBN_2_paths
+    Inception_SFusionNet_with_tdBN_2_paths, \
+    Inception_SFusionNet_ori_with_tdBN_2_ATAN
+
+from ANN_fusionnet_model import FusionNet
 import h5py
 import scipy.io as sio
 import os
@@ -25,8 +28,12 @@ from spikingjelly import visualizing
 from matplotlib import pyplot as plt
 from FSDS_code import FrequencySpectrumDistributionSimilarity
 from pytorch_model_summary import summary
+from thop import profile
 
 
+E_mac = 4.6e-12
+E_plus = 0.9e-12
+E_mul = 3.7e-12
 ###################################################################
 # ------------------- Sub-Functions (will be used) -------------------
 ###################################################################
@@ -48,7 +55,38 @@ def load_set(file_path):
 
     return gt, lms, ms, pan
 
+def cal_firing_rate(s_seq: torch.Tensor):
+    # s_seq.shape = [T, N, *]
+    # firing_rate = #spike / (neuron * T)
+    return s_seq.flatten().mean()
 
+    # return s_seq.flatten().mean()
+
+def conv(inchannel,outchannel,kernel,P,stride,H,W,batch):
+    # 一次卷积的计算量 乘加运算
+    cal = 2*kernel*kernel-1
+    # inchannel个特征图上执行卷积需要进行卷积的次数
+    times = inchannel*((H-kernel+P)/stride + 1)*((W-kernel+P)/stride + 1)
+    total = (inchannel-1)*outchannel*batch*times*cal
+    return total
+
+# 乘法
+def conv_mult(inchannel,outchannel,kernel,P,stride,H,W,batch):
+    # 一次卷积的计算量 乘运算
+    cal = kernel*kernel
+    # inchannel个特征图上执行卷积需要进行卷积的次数
+    times = inchannel*((H-kernel+P)/stride + 1)*((W-kernel+P)/stride + 1)
+    total = outchannel*batch*times*cal
+    return total
+
+# 加法
+def conv_plus(inchannel,outchannel,kernel,P,stride,H,W,batch):
+    # 一次卷积的计算量 加运算
+    cal = kernel*kernel-1
+    # inchannel个特征图上执行卷积需要进行卷积的次数
+    times = (inchannel*cal+inchannel-1)*((H-kernel+P)/stride + 1)*((W-kernel+P)/stride + 1)
+    total = outchannel*batch*times
+    return total
 
 class Dataset_Pro(data.Dataset):
     def __init__(self, file_path):
@@ -90,9 +128,8 @@ class Dataset_Pro(data.Dataset):
 ###################################################################
 # ------------------- Main Test (Run second) -------------------
 ###################################################################
-ckpt = 'A:/projects/fusionnet/80_tdbn_t_32.pth'   # chose model
+ckpt = 'A:/projects/fusionnet/250_gf2_sp1path.pth'   # chose model
 T = 32
-connect_type = 'add'
 
 def save_mat_data(sr, scale, output_dir):
     mat_dir = os.path.join(output_dir, "results")
@@ -111,16 +148,17 @@ def test(test_data_loader):
     i = 1
     s_list = []
     v_list = []
-    model = Inception_SFusionNet_with_tdBN_2_paths.FusionNet(8, 32, 8, T=T).cuda().eval()   # fixed, important!
+    model = Inception_SFusionNet_ori_with_tdBN_2_ATAN.FusionNet(4, 32, 4, T=T).cuda().eval()   # fixed, important!
     # spike_seq_monitor = monitor.OutputMonitor(model, neuron.LIFNode)
     # for param in model.parameters():
     #     param.data.abs_()
-    
+
+
 
     weight = torch.load(ckpt)  # load Weights!
     model.load_state_dict(weight) # fixed
 
-    test_folder = 'test_results_Orig_80_tdbn_t_32'
+    test_folder = 'test_rec'
     if not os.path.exists(test_folder):
         os.makedirs(test_folder)
 
@@ -130,13 +168,14 @@ def test(test_data_loader):
             # lms Nx8x64x64
             # ms_hp Nx8x16x16
             # pan_hp Nx1x64x64
-            
+            fr_monitor = monitor.OutputMonitor(model, neuron.LIFNode, cal_firing_rate)
             lms, ms_hp, pan_hp = batch[0].cuda(), batch[1].cuda(), batch[2].cuda()
-            pan_hp = input_replicate(pan_hp, 8)
+            pan_hp = input_replicate(pan_hp, 4)
             model_input = pan_hp - lms
-            print(model_input.shape)
-            print(summary(model, model_input, show_input=False, show_hierarchical=False))
-
+            # print(model_input.shape)
+            # print(summary(model, model_input, show_input=False, show_hierarchical=False))
+            macs, params = profile(model, inputs=(model_input, ))
+            print(f'macs={macs}, params={params}')
             hp_sr, lif1_out = model(model_input)  # call model
             sr = hp_sr + lms  # output:= lms + hp_sr
 
@@ -151,6 +190,109 @@ def test(test_data_loader):
             # sr = draw.linstretch(sr, 0.01, 0.99)  # fixed!
             sr = draw.to_rgb(sr)
             cv2.imwrite(test_folder + f"/{i}.png", sr*255)  # fixed! save as .png format that will used in Python!
+
+            print(f'Iteration: {iteration}')
+            print(f'fr_monitor.monitored_layers={fr_monitor.monitored_layers}')
+            print(f'fr_monitor.records=\n{fr_monitor.records}')
+
+            FL_FEE_mult = conv_mult(inchannel=4, 
+                            outchannel=32, 
+                            kernel=3, 
+                            P=2, 
+                            stride=1, 
+                            H=512, 
+                            W=512, 
+                            batch=1)
+            FL_FEE_add = conv_plus(inchannel=32,
+                            outchannel=32,
+                            kernel=3,
+                            P=2,
+                            stride=1,
+                            H=512,
+                            W=512,
+                            batch=1)
+            
+            FL_SRB1_1 = conv_plus(inchannel=32,
+                            outchannel=32,
+                            kernel=3,
+                            P=2,
+                            stride=1,
+                            H=512,
+                            W=512,
+                            batch=1)
+            FL_SRB1_2 = FL_SRB1_1
+            FL_SRB2_1 = FL_SRB1_1
+            FL_SRB2_2 = FL_SRB1_1
+            FL_SRB3_1 = FL_SRB1_1
+            FL_SRB3_2 = FL_SRB1_1
+            FL_SRB4_1 = FL_SRB1_1
+            FL_SRB4_2 = FL_SRB1_1
+
+            # FL_SRB_1_1_mul = conv_mult(inchannel=32,
+            #                 outchannel=32,
+            #                 kernel=3,
+            #                 P=2,
+            #                 stride=1,
+            #                 H=512,
+            #                 W=512,
+            #                 batch=1)
+            # FL_SRB_1_2_mul = FL_SRB_1_1_mul
+            # FL_SRB_2_1_mul = FL_SRB_1_1_mul
+            # FL_SRB_2_2_mul = FL_SRB_1_1_mul
+            # FL_SRB_3_1_mul = FL_SRB_1_1_mul
+            # FL_SRB_3_2_mul = FL_SRB_1_1_mul
+            # FL_SRB_4_1_mul = FL_SRB_1_1_mul
+            # FL_SRB_4_2_mul = FL_SRB_1_1_mul
+
+            FL_TID_1 = conv_plus(inchannel=32,
+                            outchannel=4,
+                            kernel=3,
+                            P=2,
+                            stride=1,
+                            H=512,
+                            W=512,
+                            batch=1)
+            FL_TID_2 = FL_TID_1
+
+            # FL_TID_1_mul = conv_mult(inchannel=32,
+            #                 outchannel=4,
+            #                 kernel=3,
+            #                 P=2,
+            #                 stride=1,
+            #                 H=512,
+            #                 W=512,
+            #                 batch=1)
+            
+
+            
+
+            monitor_rec = fr_monitor.records
+            fr_1 = monitor_rec[0]
+            fr_res1_1 = monitor_rec[1]
+            fr_res1_2 = monitor_rec[2]
+            fr_res2_1 = monitor_rec[3]
+            fr_res2_2 = monitor_rec[4]
+            fr_res3_1 = monitor_rec[5]
+            fr_res3_2 = monitor_rec[6]
+            fr_res4_1 = monitor_rec[7]
+            fr_res4_2 = monitor_rec[8]
+
+            E_tot = T * (FL_FEE_mult * E_mul + \
+                        FL_FEE_add * E_plus + \
+                        fr_1 * FL_SRB1_1 * E_plus + \
+                        fr_res1_1 * FL_SRB1_2 * E_plus + \
+                        fr_res1_2 * FL_SRB2_1 * E_plus + \
+                        fr_res2_1 * FL_SRB2_2 * E_plus + \
+                        fr_res2_2 * FL_SRB3_1 * E_plus + \
+                        fr_res3_1 * FL_SRB3_2 * E_plus + \
+                        fr_res3_2 * FL_SRB4_1 * E_plus + \
+                        fr_res4_1 * FL_SRB4_2 * E_plus + \
+                        fr_res4_2 * FL_TID_1 * E_plus + \
+                        fr_res4_2 * FL_TID_2 * E_plus
+            )            
+            print(f'E_tot={E_tot}')
+        
+            fr_monitor.remove_hooks()
 
             # calculate FSDS value
             # fsds_result = FrequencySpectrumDistributionSimilarity(sr, gt)
@@ -190,7 +332,8 @@ def test(test_data_loader):
 # ------------------- Main Function (Run first) -------------------
 ###################################################################
 if __name__ == '__main__':
-    test_set = Dataset_Pro('A:/pancollection/wv3/test_wv3_OrigScale_multiExm1.h5')
+    test_set = Dataset_Pro('A:/pancollection/gf2/test_gf2_OrigScale_multiExm1.h5')
+    # test_set = Dataset_Pro('A:/pancollection/gf2/test_gf2_multiExm1.h5')
     test_data_loader = DataLoader(dataset=test_set,
                                   num_workers=0,
                                   batch_size=1,
